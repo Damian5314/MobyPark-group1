d4f1ad9dfce9db64d6253575477d7a1f39ebc413
mayrii101 <161327932+mayrii101@users.noreply.github.com>
2025-12-31 17:20:46 +0100

    Update ParkingSession.cs

--- v2/Models/ParkingSession.cs
+++ v2/Models/ParkingSession.cs
@@ -16,6 +16,10 @@ public class ParkingSession
     [JsonProperty("licenseplate")]
     public string LicensePlate { get; set; } = null!;
 
+    [NotMapped]
+    [JsonProperty("user")]
+    public string Username { get; set; } = null!;
+
     [JsonProperty("started")]
     public DateTime Started { get; set; }
 
