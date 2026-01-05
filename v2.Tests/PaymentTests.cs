086195a5edb19578123295b64d075968912f5ad6
Damian5314 <damianwillmse@gmail.com>
2026-01-05 22:45:47 +0100

    Add PaymentTests class with GetAll test

---
+++ v2.Tests/PaymentTests.cs
@@ -0,0 +1,49 @@
+using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using v2.Models;
+using v2.Services;
+using Xunit;
+
+namespace v2.Tests
+{
+    public class PaymentTests : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _client;
+
+        public PaymentTests(WebApplicationFactory<Program> factory)
+        {
+            var f = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    var paymentDescriptors = services
+                        .Where(d => d.ServiceType == typeof(IPaymentService))
+                        .ToList();
+
+                    foreach (var d in paymentDescriptors)
+                        services.Remove(d);
+
+                    services.AddScoped<IPaymentService, PaymentService>();
+                });
+            });
+
+            _client = f.CreateClient();
+        }
+
+        [Fact]
+        public async Task GetAll_Should_Return_List_Of_Payments()
+        {
+            var response = await _client.GetAsync("/api/Payment");
+
+            response.EnsureSuccessStatusCode();
+            var payments = await response.Content.ReadFromJsonAsync<List<Payment>>();
+
+            payments.Should().NotBeNull();
+            payments.Should().BeOfType<List<Payment>>();
+        }
+    }
+}
